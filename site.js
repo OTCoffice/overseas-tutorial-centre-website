bef26ee842ca169c4adeb2fdbf29d33037eb93e4
Overseas Tutorial College <office@overseasuk.com>
2026-05-26 06:47:05 +0100

    Apply editorial Chinese typography sitewide

--- site.js
+++ site.js
@@ -347,7 +347,7 @@ function pageShell({ title, current = "", body, lang = "en", locale = "en", desc
   <meta name="twitter:description" content="${description}">
   ${socialImageUrl ? `<meta name="twitter:image" content="${socialImageUrl}">` : ""}
   <script type="application/ld+json">${JSON.stringify(structuredData)}</script>
-  <link rel="stylesheet" href="/styles.css?v=academic-reading-stickers-20260524">
+  <link rel="stylesheet" href="/styles.css?v=zh-editorial-fonts-20260526">
 </head>
 <body>
   ${nav(current, locale)}
