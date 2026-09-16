0758e7c88079db0134706b2b621d76a306227143
Overseas Tutorial Centre <office@overseasuk.com>
2026-09-16 16:57:00 +0100

    Add bilingual France study and settlement service

---
+++ content/france-study-work-settlement-service-en.cjs
@@ -0,0 +1,2 @@
+const render=require('./france-study-work-settlement-service-template.cjs');
+module.exports=()=>render(require('./france-study-work-settlement-service-en.json'),'en');
---
+++ content/france-study-work-settlement-service-zh.cjs
@@ -0,0 +1,2 @@
+const render=require('./france-study-work-settlement-service-template.cjs');
+module.exports=()=>render(require('./france-study-work-settlement-service-zh.json'),'zh');
